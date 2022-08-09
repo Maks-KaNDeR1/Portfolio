617beec3d2d969efb9739b3048aef841d4fd87d4
Maks-KaNDeR <makskander1@gmail.com>
2022-08-09 10:44:06 +0300

    text

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -34,9 +34,12 @@ export const Main: React.FC<PropsType> = ({ theme }) => {
                                 wrapperClassName: ''
                             }} />
                         <span style={{ fontSize: '17px', maxWidth: '605px', marginTop: '25px' }} >
-                            I'm a Frontend result-oriented developer when creating SPA applications,
-                            using JavaScript/TypeScript, React, Redux and much more in my Skills
-
+                            Hello, I'm a frontend developer in love with my business
+                            I have experience in creating SPA with React/Redux/TypeScript
+                            and much more in my Skills in Projects.
+                            I constantly strive to learn something new,
+                            improve my skills and expand them with new technologies.
+                            Open for your suggestions
                             <p /> My github: <b />
                             <a target="_blank" rel="noreferrer" href='https://github.com/Maks-KaNDeR1' >@Maks_KaNDeR</a>
                         </span>
